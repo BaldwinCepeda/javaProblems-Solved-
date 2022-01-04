a45bd4bf519ed468911319f06a4a9cf411c35fc9
Baldwin <Baldwincepeda@gmail.com>
2022-01-03 21:26:42 -0500

    first commit(testing)

---
+++ mainMethodExplanation.java
@@ -0,0 +1,25 @@
+//Name:Baldwin Cepeda
+//Description: hello this is more for myself than for the public but feel free the charm in 
+//the point of this project is for me to learn a few miscellaneous things that about java 
+//that were not so clear to me. I also be throwing anything java related in this repo 
+//if you find any bugs or thing that I miss understood while making this please feel free and contact me
+//the point of this is for me to learn and thank you for reading this :D 
+//here my contact info 
+//baldwincepeda@icloud.com
+
+
+
+public class mainMethodExplanation{
+
+
+    //this is public because the jre has to call this method thus needing it to be public
+    public static void main(String[] args)
+    {
+        int i = 0;
+        int x = 1;
+
+        System.out.println("hello world" + (x+i));
+    }
+
+
+}
